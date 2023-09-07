9a367f95445a6196955e4a2b4610efef23518750
PSKutergin <PSKutergin@gmail.com>
2023-09-07 14:52:17 +0300

    lesson06 hard task

--- js/script.js
+++ js/script.js
@@ -2,29 +2,52 @@
 
 let main = () => {
   let num = parseInt(Math.random() * 100)
-  return function result() {
+
+  return function result(attempt) {
+    let again
+    let userNum
+
+    if (attempt === 0) {
+      again = confirm('Попытки закончились, хотите сыграть еще?')
+      if (again) {
+        num = parseInt(Math.random() * 100)
+        result(10)
+      } else {
+        return
+      }
+    }
+
     userNum = prompt('Угадай число от 1 до 100')
 
     if (isNaN(userNum) || userNum === '') {
       alert('Введи число!')
-      result()
+      result(attempt)
     }
     if (userNum === null) {
       alert('Игра окончена')
       return
     }
+
+    attempt--
+    console.log(attempt, num)
+
     if (num === +userNum) {
-      alert('Поздравляю, Вы угадали!!!')
-      return
+      again = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')
+      if (again) {
+        num = parseInt(Math.random() * 100)
+        result(10)
+      } else {
+        return
+      }
     } else if (num > +userNum) {
-      alert('Загаданное число больше')
-      result()
+      alert('Загаданное число больше, осталось попыток ' + attempt)
+      result(attempt)
     } else if (num < +userNum) {
-      alert('Загаданное число меньше')
-      result()
+      alert('Загаданное число меньше, осталось попыток ' + attempt)
+      result(attempt)
     }
   }
 }
 
 let start = main()
-start()
+start(10)
